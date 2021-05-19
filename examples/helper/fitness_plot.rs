9b9e85de32f6113939e43d36c7d8eb4988d4b71f
Suh Donghwi <hwidongsuh@gmail.com>
2021-05-20 00:36:13 +0900

    feat: add horizontal line drawing

--- examples/helper/fitness_plot.rs
+++ examples/helper/fitness_plot.rs
@@ -1,3 +1,4 @@
+use float_cmp::ApproxEq;
 use ggez::nalgebra as na;
 use ggez::{graphics, mint};
 
@@ -68,6 +69,31 @@ impl FitnessPlot {
         )
     }
 
+    fn draw_horizontal(
+        &self,
+        ctx: &mut ggez::Context,
+        y: f32,
+        fitness: f32,
+        rect: &graphics::Rect,
+    ) -> ggez::GameResult<()> {
+        let line = graphics::Mesh::new_line(
+            ctx,
+            &[na::Point2::new(0.0, y), na::Point2::new(rect.w, y)],
+            1.5,
+            graphics::Color::from_rgba(0, 0, 0, 50),
+        )?;
+
+        graphics::draw(ctx, &line, (rect.point(),))?;
+
+        let text = Text::new(ctx, &format!("{:.1}", fitness), 28.0);
+        let width = text.width(ctx);
+        text.draw(
+            ctx,
+            na::Point2::new(rect.x - width as f32 + 7.0, rect.y + y - 7.0),
+            graphics::BLACK,
+        )
+    }
+
     pub fn draw(&self, ctx: &mut ggez::Context) -> ggez::GameResult<()> {
         if self.fitness_list.len() < 2 {
             return Ok(());
@@ -95,12 +121,6 @@ impl FitnessPlot {
             .cloned()
             .collect();
 
-        let delta = if to_show.len() <= 1 {
-            0.0
-        } else {
-            actual_rect.w / (to_show.len() - 1) as f32
-        };
-
         let max = 4.0;
         let min = 2.0;
         let current_gen = self.fitness_list.len();
@@ -108,7 +128,7 @@ impl FitnessPlot {
         self.draw_axis(ctx, &actual_rect)?;
 
         let gen_delta = (to_show.len() as f64 / 4.0).ceil() as usize;
-        let y_delta = 0.5;
+        let fitness_delta = 1.0;
 
         let gen_start = if self.fitness_list.len() < max_points {
             0
@@ -122,6 +142,24 @@ impl FitnessPlot {
             gen += gen_delta;
         }
         self.draw_vertical(ctx, current_gen, actual_rect.w, &actual_rect)?;
+
+        let mut fitness: f32 = 0.0;
+        while !fitness.approx_eq(max, (0.0, 2)) {
+            self.draw_horizontal(
+                ctx,
+                actual_rect.h - actual_rect.h * fitness / max,
+                fitness,
+                &actual_rect,
+            )?;
+            fitness += fitness_delta;
+        }
+        self.draw_horizontal(ctx, 0.0, max, &actual_rect)?;
+
+        let delta = if to_show.len() <= 1 {
+            0.0
+        } else {
+            actual_rect.w / (to_show.len() - 1) as f32
+        };
 
         let points: Vec<na::Point2<f32>> = to_show
             .iter()
